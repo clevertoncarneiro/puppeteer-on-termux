12fbce81a0e97d3c88fda83216eb81a62ecec3f8
clevertoncarneiro <cleverton.carneiro@agres.com.br>
2022-02-20 13:27:48 -0300

    Make script to get and save a page periodically

--- index.js
+++ index.js
@@ -1,30 +1,58 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-screenshot('https://example.com').then(() => console.log('screenshot saved'));
+async function sourcecode(url) {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: [
+            "--no-sandbox",
+            "--disable-gpu",
+        ]
+    });
 
-async function screenshot(url) {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: [
-      "--no-sandbox",
-      "--disable-gpu",
-    ]
-  });
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.goto(url, {
+        timeout: 0,
+        waitUntil: 'domcontentloaded', // 'domcontentloaded' || 'networkidle0'
+    });
+    const pageSource = await page.content();
+    if (!!pageSource) {
+        fs.writeFileSync(process.argv[2] + 'source.html', pageSource);
+    } else {
+        throw Error('Unable get source');
+    }
 
-  const page = await browser.newPage();
-  await page.setViewport({width: 1920, height: 1080});
-  await page.goto(url, {
-    timeout: 0,
-    waitUntil: 'networkidle0',
-  });
-  const screenData = await page.screenshot({encoding: 'binary', type: 'jpeg', quality: 100});
-  if (!!screenData) {
-    fs.writeFileSync('screenshots/screenshot.jpg', screenData);
-  } else {
-    throw Error('Unable to take screenshot');
-  }
-
-  await page.close();
-  await browser.close();
+    await page.close();
+    await browser.close();
 }
+
+function wait(sec) {
+    return new Promise(resolve => {
+        setTimeout(() => { resolve('') }, 1000 * sec);
+    })
+}
+
+function checkarguments() {
+    try {
+        if (process.argv[2].length < 5) {
+            throw Error('Missing path argument to save source.');
+        }
+    } catch (error) {
+        throw Error('Missing path argument to save source.');
+    }
+}
+
+async function mainloop(url, waittime) {
+    checkarguments();
+
+    while (true) {
+        await wait(waittime);
+        sourcecode(url);//.then(() => console.log('source saved'));
+    }
+}
+
+
+
+// mainloop(website, seconds)
+mainloop('https://www.pelando.com.br/recentes', 20);
